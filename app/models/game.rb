eb74662e68a833eae20c693d75aea2e62ddb9280
Pei Liu <peixiong.liu@gmail.com>
2020-04-05 16:16:06 -0400

    Finish Game#self.new_board

--- app/models/game.rb
+++ app/models/game.rb
@@ -4,4 +4,30 @@ class Game < ApplicationRecord
     presence: true,  
     inclusion: { in: %w(active inactive), message: "%{value} is not a valid status" }
   validates :state, presence: true
+
+  def self.new_board(special_deck = nil)
+    default_deck = load_deck('default')
+    special_deck = load_deck(special_deck)
+
+    cards = default_deck.sample(25)
+
+    if special_deck
+      # Replace some cards with special cards.
+      cards.shift(4)
+      cards.concat(special_deck.sample(4))
+      cards.shuffle!
+    end
+
+    cards
+  end
+
+  private
+
+  def self.load_deck(deck_name)
+    path = Rails.root.join("app/assets/decks/#{deck_name}.txt")
+
+    return unless deck_name && File.exist?(path)
+
+    File.read(path).split("\n").map(&:upcase)
+  end
 end
