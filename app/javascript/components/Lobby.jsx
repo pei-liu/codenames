bddd8ebf457924e881b7b383d56454f762b266c0
Pei Liu <peixiong.liu@gmail.com>
2020-04-05 19:45:23 -0400

    Get form to alert

--- app/javascript/components/Lobby.jsx
+++ app/javascript/components/Lobby.jsx
@@ -1,23 +1,52 @@
 import React from "react";
 import { Link } from "react-router-dom";
 
-export default () => (
-  <div className="vw-100 vh-100 primary-color d-flex align-items-center justify-content-center">
-    <div className="jumbotron jumbotron-fluid bg-transparent">
-      <div className="container secondary-color">
-        <h1 className="display-4">Codenames2</h1>
-        <p className="lead">
-          Codenames, 2
-        </p>
-        <hr className="my-4" />
-        <Link
-          to="/recipes"
-          className="btn btn-lg custom-button"
-          role="button"
-        >
-          View Recipes
-        </Link>
+class Lobby extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      identifier: ''
+    }
+
+    this.onInputChange = this.onInputChange.bind(this);
+    this.onSubmit = this.onSubmit.bind(this);
+  }
+
+  onInputChange(event) {
+    this.setState({identifier: event.target.value});
+  }
+
+  onSubmit(event) {
+    alert(this.state.identifier);
+    event.preventDefault();
+  }
+
+  render() {
+    return (
+      <div className="primary-color d-flex align-items-center justify-content-center">
+        <div className="jumbotron jumbotron-fluid bg-transparent">
+          <div className="container secondary-color">
+            <p className="lead">
+              Enter Game ID to join or create game!
+            </p>
+            <form onSubmit={this.onSubmit} className="text-center">
+              <div className="form-group">
+                <input 
+                  type="text" 
+                  className="form-control" 
+                  id="identifierFormInput" 
+                  placeholder="Enter identifer" 
+                  value={this.state.value}
+                  onChange={this.onInputChange} 
+                />
+              </div>
+              <input type="submit" className="btn btn-primary" value="Submit" />
+            </form>
+          </div>
+        </div>
       </div>
-    </div>
-  </div>
-);
+    );
+  }
+}
+
+export default Lobby;
