28aa96518a664e6ea907989424c06c459b969c33
Pei Liu <peixiong.liu@gmail.com>
2020-04-05 16:45:47 -0400

    Actually finish Game#self.new_board

--- Gemfile
+++ Gemfile
@@ -28,6 +28,8 @@ gem 'jbuilder', '~> 2.7'
 # Reduces boot times through caching; required in config/boot.rb
 gem 'bootsnap', '>= 1.4.2', require: false
 
+gem 'awesome_print'
+
 group :development, :test do
   gem 'pry'
 end
--- app/models/game.rb
+++ app/models/game.rb
@@ -1,11 +1,48 @@
 class Game < ApplicationRecord
+  RED = 'red'
+  BLUE = 'blue'
+  NEUTRAL = 'neutral'
+  ASSASSIN = 'assassin'
+
   validates :identifier, presence: true, uniqueness: true
   validates :status, 
     presence: true,  
     inclusion: { in: %w(active inactive), message: "%{value} is not a valid status" }
   validates :state, presence: true
 
-  def self.new_board(special_deck = nil)
+  def self.new_board(going_first = RED, special_deck = nil)
+    raise ArgumentError unless [RED, BLUE].include? going_first
+
+    red_count, blue_count = going_first == RED ? [9, 8] : [8, 9]
+    assassin_count = 1
+
+    board = pick_cards(special_deck).map do |card|
+      type = if red_count > 0
+               red_count -= 1
+               RED
+             elsif blue_count > 0
+               blue_count -= 1
+               BLUE
+             elsif assassin_count > 0
+               assassin_count -= 1
+               ASSASSIN
+             else
+               NEUTRAL
+             end
+      {
+        title: card,
+        type: type,
+        is_selected: false
+      }
+    end
+
+    board.shuffle
+  end
+
+  private
+
+  # returns Array of 25 random/shuffled cards
+  def self.pick_cards(special_deck)
     default_deck = load_deck('default')
     special_deck = load_deck(special_deck)
 
@@ -20,9 +57,7 @@ class Game < ApplicationRecord
 
     cards
   end
-
-  private
-
+  
   def self.load_deck(deck_name)
     path = Rails.root.join("app/assets/decks/#{deck_name}.txt")
 
