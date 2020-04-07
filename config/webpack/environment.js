620b0d6a6a64664dcb622ec8a17a1939ba1b56b5
Pei Liu <peixiong.liu@gmail.com>
2020-04-07 00:08:22 -0400

    Comment

--- config/webpack/environment.js
+++ config/webpack/environment.js
@@ -2,5 +2,6 @@ const { environment } = require('@rails/webpacker')
 
 module.exports = environment
 
+// TO DO: Figure out how to not need this line.
 // Fixes action cable error: https://github.com/rails/rails/issues/35501
 environment.loaders.delete('nodeModules')
