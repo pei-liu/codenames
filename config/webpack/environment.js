99ed9d3069ceeffd3e4455fa46bc6be6ae032c84
Pei Liu <peixiong.liu@gmail.com>
2020-04-07 00:08:00 -0400

    Hack to fix actioncable error

--- config/webpack/environment.js
+++ config/webpack/environment.js
@@ -1,3 +1,6 @@
 const { environment } = require('@rails/webpacker')
 
 module.exports = environment
+
+// Fixes action cable error: https://github.com/rails/rails/issues/35501
+environment.loaders.delete('nodeModules')
