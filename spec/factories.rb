306e801abe840748f20e2357f443730db0165a57
Pei Liu <peixiong.liu@gmail.com>
2020-04-13 21:49:41 -0400

    FactoryBot working

--- spec/factories.rb
+++ spec/factories.rb
@@ -1,7 +1,5 @@
 FactoryBot.define do
   factory :game do
     identifier { "asdf" }
-    status { "active" }
-    state {  }
   end
 end
--- spec/models/game_spec.rb
+++ spec/models/game_spec.rb
@@ -1,6 +1,10 @@
 require 'rails_helper'
 
 RSpec.describe Game, type: :model do
+
+  let(:game) { build(:game) }
   it 'asdf' do
+    binding.pry
   end
+
 end
