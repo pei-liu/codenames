61a59ff21ec838865b1880a9b83128eb1bab24da
Pei Liu <peixiong.liu@gmail.com>
2020-04-23 22:19:29 -0400

    Have logo link to home

--- app/javascript/routes/Index.jsx
+++ app/javascript/routes/Index.jsx
@@ -1,10 +1,11 @@
 import React from "react";
-import { BrowserRouter as Router, Route, Switch } from "react-router-dom";
+import { BrowserRouter as Router, Route, Switch, Link } from "react-router-dom";
 import Lobby from "../components/Lobby";
 import Game from "../components/Game";
 
 export default (
   <Router>
+    <h1 id='logo'><Link to="/">Codenames</Link></h1>
     <Switch>
       <Route path="/" exact component={Lobby} />
       <Route path="/secretlobby" exact component={Lobby} />
