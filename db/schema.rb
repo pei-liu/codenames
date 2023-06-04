a7f4f34c729069655ba2894fe2e34b2149167dd4
Pei Liu <peixiong.liu@gmail.com>
2023-06-03 23:15:46 -0400

    upgrade to webpack 5

--- db/schema.rb
+++ db/schema.rb
@@ -2,16 +2,15 @@
 # of editing this file, please use the migrations feature of Active Record to
 # incrementally modify your database, and then regenerate this schema definition.
 #
-# This file is the source Rails uses to define your schema when running `rails
-# db:schema:load`. When creating a new database, `rails db:schema:load` tends to
+# This file is the source Rails uses to define your schema when running `bin/rails
+# db:schema:load`. When creating a new database, `bin/rails db:schema:load` tends to
 # be faster and is potentially less error prone than running all of your
 # migrations from scratch. Old migrations may fail to apply correctly if those
 # migrations use external dependencies or application code.
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2020_12_19_160917) do
-
+ActiveRecord::Schema[7.0].define(version: 2020_12_19_160917) do
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
 
@@ -19,8 +18,8 @@ ActiveRecord::Schema.define(version: 2020_12_19_160917) do
     t.string "name", null: false
     t.boolean "is_private", null: false
     t.integer "num_cards_included", default: 4, null: false
-    t.datetime "created_at", precision: 6, null: false
-    t.datetime "updated_at", precision: 6, null: false
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
     t.index ["name", "is_private"], name: "index_decks_on_name_and_is_private", unique: true
   end
 
@@ -28,8 +27,8 @@ ActiveRecord::Schema.define(version: 2020_12_19_160917) do
     t.string "identifier", null: false
     t.string "status", default: "active", null: false
     t.jsonb "state"
-    t.datetime "created_at", precision: 6, null: false
-    t.datetime "updated_at", precision: 6, null: false
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
     t.string "seen_cards", default: ""
     t.bigint "custom_deck_id"
     t.integer "num_cards_included_override"
