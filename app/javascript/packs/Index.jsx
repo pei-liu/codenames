c05e1f0de3d9a6f1cde40381eab95be5d5ba29cb
Pei Liu <peixiong.liu@gmail.com>
2023-12-30 20:04:27 -0500

    Upgrade to react 18

--- app/javascript/packs/Index.jsx
+++ app/javascript/packs/Index.jsx
@@ -1,10 +1,11 @@
 import React from "react";
-import { render } from "react-dom";
+import { createRoot } from 'react-dom/client';
 import App from "../components/App";
 
 document.addEventListener("DOMContentLoaded", () => {
-  render(
-    <App />,
-    document.body.appendChild(document.createElement("div"))
+  const container = document.body.appendChild(document.createElement("div"));
+  const root = createRoot(container);
+  root.render(
+    <App />
   );
 });
